394fbfa974e9073456a9a16629bec5ceaf824c66
Nguyễn Võ Song Toàn <adamwilling.2002@gmail.com>
2021-08-22 20:09:52 +0700

    sS

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,30 @@ window.addEventListener("scroll", () => {
 
 
 // Khởi tạo danh sách users
-let users_state = []
+let users_default = [
+    {
+        company: "Ký Túc Xá Cỏ May",
+        name: "Võ Thành Phú",
+        date: "08/02/2001",
+        email: "thanhphums10@gmail.com",
+        phone: "0775074059",
+        password: "Abc123456",
+        department: "Đội sửa chữa",
+        position: "Thành viên",
+        set: "member"
+    },
+    {
+        company: "Ký Túc Xá Cỏ May",
+        name: "Nguyễn Võ Song Toàn",
+        date: "08/02/2001",
+        email: "adamwilling.2002@gmail.com",
+        phone: "0369572542",
+        password: "Abc123456",
+        department: "Tầng trệt",
+        position: "Trưởng phòng 018",
+        set: "admin"
+    }
+]
 if (JSON.parse(localStorage.getItem("users")) == null) {
-    window.localStorage.setItem("users","[]")
+    window.localStorage.setItem("users",JSON.stringify(users_default))
 }
